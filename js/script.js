16a95a19217242e84c2e0783bfaa1f848fc63ac0
claudio <claudio.cenghialta@gmail.com>
2020-07-28 17:25:04 +0200

    struttura base

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+Replica della grafica con la possibilità 
+di avere messaggi scritti dall'utente (verdi) e
+dall'interlocutore (bianco) 
+assegnando due classi CSS diverse.
+*/
+
+$(document).ready(function(){
+    
+
+
+
+
+
+
+});
