f674550c43f3df45fc46f8cd2ca27de66c4e8ece
Claudio Cenghialta <claudio.cenghialta@gmail.com>
2020-07-29 16:34:35 +0200

    milestone 2 completata

--- js/script.js
+++ js/script.js
@@ -4,13 +4,85 @@ di avere messaggi scritti dall'utente (verdi) e
 dall'interlocutore (bianco) 
 assegnando due classi CSS diverse.
 */
+var arrayRisposte = [
+    'ok',
+    'non saprei',
+    'certamente',
+    'assolutamente no',
+    'ciao',
+    'buona giornata'
+];
 
-$(document).ready(function(){
-    
 
-
-
-
-
-
+$(document).ready(function () {
+    //mostra microfono o invio messaggio a seconda del focus su input
+    // + "sta scrivendo" sotto nome utente
+    $("#input-message").focus(function () {
+        $('#nuovo-messaggio .icona-invio ').toggleClass('active');
+        $('#app-right .header-left .avatar-details').toggleClass('active');
+    })
+    $("#input-message").focusout(function () {
+        setTimeout(function () {
+            $('#nuovo-messaggio .icona-invio ').toggleClass('active');
+            $('#app-right .header-left .avatar-details').toggleClass('active');
+        }, 100);
+    })
+    //al click o quando premo invio inizia la chat
+    $('#nuovo-messaggio i.fa-paper-plane').click(function () {
+        chatta();
+    })
+    $("#input-message").keydown(function (e) {
+        if (e.which == 13) {
+            chatta();
+        }
+    });
 });
+
+//dopo 1 secondo genero un messaggio di risposta
+
+
+
+//FUNZIONI
+
+//CHAT 
+function chatta() {
+    //prendo il valore del messaggio nella input
+    var nuovoMessaggio = $('#input-message').val()
+    if (nuovoMessaggio != '') {
+        //NB escludo invio quando input è vuota
+        invioMessaggio(nuovoMessaggio, 'sent')
+        setTimeout(function () {
+            invioMessaggio(arrayRisposte[random(0, arrayRisposte.length - 1)])
+        }, 1000);
+        $('#input-message').val('')
+    }
+}
+//INVIO MESSAGGIO
+function invioMessaggio(testo, tipo) {
+    //clono template ed inserisco valore testo + orario
+    var elemento = $('#templates .message').clone();
+    elemento.find('.message-text').append(testo);
+    elemento.find('.message-hour').append(oraEsatta);
+    if (tipo == 'sent') {
+        elemento.addClass('sent')
+    } else {
+        elemento.addClass('received')
+    }
+    $('#chat').append(elemento);
+}
+
+//ORA ESATTA
+function oraEsatta() {
+    var d = new Date();
+    var ore = d.getHours()
+    var minuti = d.getMinutes()
+    if (minuti < 10) {
+        minuti = '0' + minuti
+    }
+    return ore + ':' + minuti;
+}
+
+//RANDOM
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
