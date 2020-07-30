7d5d1849066a8657a0e8fda1aebb4fa243a1e3c3
Claudio Cenghialta <claudio.cenghialta@gmail.com>
2020-07-30 11:49:49 +0200

    milestone 3 ok - passare da una chat ad un'altra

--- js/script.js
+++ js/script.js
@@ -27,6 +27,19 @@ $(document).ready(function () {
             $('#app-right .header-left .avatar-details').toggleClass('active');
         }, 100);
     })
+    $('.box-avatar').click(function () {
+        if (!$(this).hasClass('active')) {
+            //sposto la classe active nel box avatar selezionato
+            $('.box-avatar').removeClass('active');
+            $(this).addClass('active');
+            //calcolo posizione nell'elenco
+            var posizione = $(this).index();
+            console.log(posizione);
+            //sposto la classe active nel relativo chat-messages 
+            $('.chat-messages').removeClass('active');
+            $('.chat-messages').eq(posizione).addClass('active');
+        }
+    })
     //al click o quando premo invio inizia la chat
     $('#nuovo-messaggio i.fa-paper-plane').click(function () {
         chatta();
@@ -51,18 +64,24 @@ function chatta() {
         //NB escludo invio quando input è vuota
         invioMessaggio(nuovoMessaggio, 'sent');
         //faccio scroll dei messaggi in automatico
-        var posizione = $('#chat .message:last-child').position();
-        $('#chat').scrollTop(posizione.top);
+        scrollChat();
         //dopo 1 secondo genero un messaggio di risposta
         setTimeout(function () {
             //risposta presa random da un array di risposte prestabilite
-            invioMessaggio(arrayRisposte[random(0, arrayRisposte.length - 1)])
-            $('#chat').scrollTop(posizione.top);
+            invioMessaggio(arrayRisposte[random(0, arrayRisposte.length - 1)]);
+            scrollChat();
         }, 1000);
         //reset campo input
         $('#input-message').val('');
     }
 }
+
+//SCROLL AUTOMATICO MESSAGGI CHAT
+function scrollChat() {
+    var posizione = $('#chat .chat-messages.active .message:last-child').position();
+    $('#chat').scrollTop(posizione.top);
+}
+
 //INVIO MESSAGGIO
 function invioMessaggio(testo, tipo) {
     //clono template ed inserisco valore testo + orario
@@ -74,7 +93,7 @@ function invioMessaggio(testo, tipo) {
     } else {
         elemento.addClass('received')
     }
-    $('#chat').append(elemento);
+    $('#chat .chat-messages.active').append(elemento);
 }
 
 //ORA ESATTA
